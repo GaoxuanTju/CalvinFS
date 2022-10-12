54df41dde8a026d49fa05dbdd96403e699e1fe61
gaoxuan <253877113@qq.com>
2022-10-12 14:21:01 +0800

    14:20

--- src/fs/metadata_store.cc
+++ src/fs/metadata_store.cc
@@ -520,8 +520,9 @@ void MetadataStore::GetRWSets(Action* action) {//gaoxuan --this function is call
             //get all children and push them into stack
 
             string metadata_entry;//gaoxuan --used to get metadata in the format of string
-            LOG(ERROR)<<MetadataStore::store_->Get(in.from_path(),100,&metadata_entry);//the 10 is version
             
+            LOG(ERROR)<<in.from_path()<<";"<<MetadataStore::store_->Get(in.from_path(),100,&metadata_entry);//the 10 is version
+        
             if(!metadata_entry.empty())//this if/else is necessary,because if metadata_entry is null,ParseFromString will be error!
             {
               MetadataEntry entry;
