fe6ce823820d62ecff024612f07d6f626b0449d4
gaoxuan <253877113@qq.com>
2022-10-12 15:06:35 +0800

    15:06

--- src/fs/metadata_store.cc
+++ src/fs/metadata_store.cc
@@ -552,8 +552,8 @@ void MetadataStore::GetRWSets(Action* action) {//gaoxuan --this function is call
     MetadataEntry entry1;
     MetadataEntry entry2;
     string str1,str2;
-    LOG(ERROR)<<store_->Get("/a0/b980/c0",100,str1);
-    LOG(ERROR)<<store_->Get("/a0/b111/c1",100,str2);
+    LOG(ERROR)<<store_->Get("/a0/b980/c0",100,&str1);
+    LOG(ERROR)<<store_->Get("/a0/b111/c1",100,&str2);
 
     //gaoxuan -test
 
