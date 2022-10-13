6843a90e565c159f5f372804e76d781fafe82932
gaoxuan <253877113@qq.com>
2022-10-13 09:10:58 +0800

    9:10

--- src/fs/metadata_store.cc
+++ src/fs/metadata_store.cc
@@ -501,15 +501,13 @@ void MetadataStore::GetRWSets(Action* action) {//gaoxuan --this function is call
     action->add_writeset(ParentDir(in.to_path()));
 
   }
-  /* else if (type == MetadataAction::RENAME) {
+  /**/ else if (type == MetadataAction::RENAME) {
     //gaoxuan --this part is rewrited by gaoxuan
     //add read/write set in the way of DFS
     MetadataAction::RenameInput in;
     in.ParseFromString(action->input());
     std::stack<string> stack;//the stack is used for tranversing  the file tree
     stack.push(in.from_path());//we want to tranverse all children of this path to add read/write set
-    action->add_readset(ParentDir(in.from_path()));
-    action->add_writeset(ParentDir(in.from_path()));
     while (!stack.empty()) 
     {
             string top = stack.top();//get the top
@@ -528,6 +526,7 @@ void MetadataStore::GetRWSets(Action* action) {//gaoxuan --this function is call
             {
               MetadataEntry entry;
               entry.ParseFromString(metadata_entry);//now all the children of from_path has been stored in entry
+              LOG(ERROR)<<entry.type();
               for(int i=0;i<entry.dir_contents_size();i++)//push all children path into stack 
               {
                   stack.push(entry.dir_contents(i));
@@ -536,27 +535,20 @@ void MetadataStore::GetRWSets(Action* action) {//gaoxuan --this function is call
             }
             
      }
+    action->add_readset(ParentDir(in.from_path()));
+    action->add_writeset(ParentDir(in.from_path()));
     //the read/write set for to_path
+
     action->add_writeset(in.to_path());
     action->add_readset(ParentDir(in.to_path()));
     action->add_writeset(ParentDir(in.to_path()));
 
   }
-  */
-  /*gaoxuan --this part is the original code*/
+  
+  /*gaoxuan --this part is the original code
   else if (type == MetadataAction::RENAME) {
     MetadataAction::RenameInput in;
     in.ParseFromString(action->input());
-
-    //gaoxuan --test
-    MetadataEntry entry1;
-    MetadataEntry entry2;
-    string str1,str2;
-    LOG(ERROR)<<store_->Get("/a0/b980/c0",100,&str1);
-    //LOG(ERROR)<<store_->Get("/a0/b111/c1",100,&str2);
-
-    //gaoxuan -test
-
     action->add_readset(in.from_path());
     action->add_writeset(in.from_path());
     action->add_readset(ParentDir(in.from_path()));
@@ -566,7 +558,7 @@ void MetadataStore::GetRWSets(Action* action) {//gaoxuan --this function is call
     action->add_writeset(ParentDir(in.to_path()));
 
   }
-   
+  */ 
    else if (type == MetadataAction::LOOKUP) {
     MetadataAction::LookupInput in;
     in.ParseFromString(action->input());
