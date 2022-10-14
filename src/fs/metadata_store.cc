94ba9fabe7f3fec16a964c0e7d08413e5a939669
gaoxuan <253877113@qq.com>
2022-10-14 21:42:31 +0800

    no idea, just try

--- src/fs/metadata_store.cc
+++ src/fs/metadata_store.cc
@@ -581,10 +581,10 @@ void MetadataStore::GetRWSets(Action* action) {//gaoxuan --this function is call
     // Run if local.
     if (IsLocal(in.from_path())) {//gaoxuan --本地的话直接用Get取出
       string metadata_entry1;
-      LOG(ERROR)<<in.from_path()<<";"<<store_->Get(in.from_path(),10,&metadata_entry1)<<";"<<metadata_entry1.size()<<":"<<metadata_entry1;
+      LOG(ERROR)<<in.from_path()<<";"<<store_->Get(path,10,&metadata_entry1)<<";"<<metadata_entry1.size()<<":"<<metadata_entry1;
 
     // If not local, get result from the right machine (within this replica).
-    }else {
+    }else {//事实上我使用这个RPC，还是会去调用Get，结果还是错误了
       LOG(ERROR)<<"rpc --"<<in.from_path();
       Header* header = new Header();
       header->set_from(machine_->machine_id());
