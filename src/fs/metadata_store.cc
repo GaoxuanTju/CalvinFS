be41b5cb293f5713e91b7fc50fc95e8f6f009cf1
gaoxuan <253877113@qq.com>
2022-10-16 20:49:35 +0800

    check

--- src/fs/metadata_store.cc
+++ src/fs/metadata_store.cc
@@ -567,9 +567,9 @@ void MetadataStore::GetRWSets(Action* action) {//gaoxuan --this function is call
     action->add_writeset(ParentDir(in.to_path()));
 
 //gaoxuan --test
-    const Slice path = Slice(ParentDir(in.from_path()));
+    //const Slice path = Slice(ParentDir(in.from_path()));
     // Find out what machine to run this on.
-    uint64 mds_machine =config_->LookupMetadataShard(config_->HashFileName(path), config_->LookupReplica(machine_->machine_id()));
+    uint64 mds_machine =config_->LookupMetadataShard(config_->HashFileName(Slice(ParentDir(in.from_path()))), config_->LookupReplica(machine_->machine_id()));
 
     // Run if local.
     if (mds_machine == machine_->machine_id()) {//gaoxuan --本地的话直接用Get取出,当前来看是能够取出来的
@@ -582,12 +582,12 @@ void MetadataStore::GetRWSets(Action* action) {//gaoxuan --this function is call
       header->set_from(machine_->machine_id());
       header->set_to(mds_machine);
       header->set_type(Header::RPC);
-      header->set_app(getAPPname());//刚刚是client，看看会不会有区别
+      header->set_app(getAPPname());//刚刚是client，看看会不会有区别,没区别
       header->set_rpc("LOOKUP");
-      LOG(ERROR)<<"chuan jin qu de lu jing shi "<<path.data();
-      header->add_misc_string(path.data(), path.size());
+      LOG(ERROR)<<"chuan jin qu de lu jing shi "<<Slice(ParentDir(in.from_path()));
+      header->add_misc_string(Slice(ParentDir(in.from_path())), Slice(ParentDir(in.from_path())));
 
-      //LOG(ERROR)<<"chuan jin qu de lu jing shi "<<header->misc_string(0);//加这一行的目的主要是看一下为啥机器1会Get的key会变成LOOKUP，确是在这里就变成了LOOKUP
+      LOG(ERROR)<<"chuan jin qu de lu jing shi "<<header->misc_string(0);//加这一行的目的主要是看一下为啥机器1会Get的key会变成LOOKUP，确是在这里就变成了LOOKUP
       MessageBuffer* m = NULL;
       header->set_data_ptr(reinterpret_cast<uint64>(&m));
       machine_->SendMessage(header, new MessageBuffer());
