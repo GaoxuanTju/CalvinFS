fc9ba75611b4b0dc4abe4d318c4ee7e001b55ed7
gaoxuan <253877113@qq.com>
2022-10-17 10:11:27 +0800

    find error

--- src/components/store/versioned_kvstore.cc
+++ src/components/store/versioned_kvstore.cc
@@ -248,7 +248,7 @@ bool VersionedKVStore::Get(
     }
     if (it->Key()[key.size()] != '\0') {
       delete it;
-      LOG(ERROR)<<it->Key()[key.size()];
+      LOG(ERROR)<<it->Key();
       LOG(ERROR)<<key<<";gaoxuan --false 3";//gaoxuan --all false is from here
       return false;
     }
