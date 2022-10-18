a2a68ea40415a86dbacb0946871ee4b9ca00e700
gaoxuan <253877113@qq.com>
2022-10-18 16:26:53 +0800

    change it to DIR

--- src/fs/calvinfs_client_app.h
+++ src/fs/calvinfs_client_app.h
@@ -695,8 +695,11 @@ void LatencyExperimentAppend() {
       while (a2 == a1) {
         a2 = rand() % 1000;
       }
-      BackgroundRenameFile("/a" + IntToString(machine()->machine_id()) + "/b" + IntToString(a1) + "/c" + IntToString(j),
-                           "/a" + IntToString(rand() % machine()->config().size()) + "/b" + IntToString(a2) + "/d" + IntToString(machine()->GetGUID())); 
+      /*BackgroundRenameFile("/a" + IntToString(machine()->machine_id()) + "/b" + IntToString(a1) + "/c" + IntToString(j),
+                           "/a" + IntToString(rand() % machine()->config().size()) + "/b" + IntToString(a2) + "/d" + IntToString(machine()->GetGUID())); */
+      //gaoxuan --change it to DIR
+      BackgroundRenameFile("/a" + IntToString(machine()->machine_id()) + "/b" + IntToString(a1) ,
+                           "/a" + IntToString(rand() % machine()->config().size()) + "/d" + IntToString(a2)); 
 
       // contention-free workload
       /**BackgroundRenameFile("/a" + IntToString(machine()->machine_id()) + "/b" + IntToString(j) + "/c" + IntToString(j),
