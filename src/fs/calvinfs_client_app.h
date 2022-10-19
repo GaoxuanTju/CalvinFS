f381a60e0cf40435e97eaceb4092cd262a0133d2
gaoxuan <253877113@qq.com>
2022-10-19 08:50:47 +0800

    change to_path

--- src/fs/calvinfs_client_app.h
+++ src/fs/calvinfs_client_app.h
@@ -699,7 +699,7 @@ void LatencyExperimentAppend() {
                            "/a" + IntToString(rand() % machine()->config().size()) + "/b" + IntToString(a2) + "/d" + IntToString(machine()->GetGUID())); */
     
       BackgroundRenameFile("/a" + IntToString(machine()->machine_id()) + "/b" + IntToString(a1) ,
-                           "/a" + IntToString(rand() % machine()->config().size()) + "/b" + IntToString(a2)) ;
+                           "/a" + IntToString(rand() % machine()->config().size()) + "/d" + IntToString(a2)) ;
       // contention-free workload
       /**BackgroundRenameFile("/a" + IntToString(machine()->machine_id()) + "/b" + IntToString(j) + "/c" + IntToString(j),
                              "/a" + IntToString((machine()->machine_id()+1)%9) + "/b" + IntToString(j+250) + "/d" + IntToString(machine()->GetGUID()));**/
