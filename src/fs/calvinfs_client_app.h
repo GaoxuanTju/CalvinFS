b424046399212e95ad1bd5f820a6d3417994adbe
gaoxuan <253877113@qq.com>
2022-10-24 22:07:37 +0800

    check error

--- src/fs/calvinfs_client_app.h
+++ src/fs/calvinfs_client_app.h
@@ -690,7 +690,7 @@ void LatencyExperimentAppend() {
     double start = GetTime();
     string from_path;
     string to_path;
-    for (int j = 0; j < 1; j++) {
+    for (int j = 0; j < 250; j++) {
       int a1 = rand() % 1000;
       int a2 = rand() % 1000;
       while (a2 == a1) {
@@ -753,7 +753,7 @@ void LatencyExperimentAppend() {
 
   
     //gaoxuan --In this part I want to get all path to check if we rename successfully,in real use,we need to delete it
-    metadata_->getLOOKUP(from_path);
+    metadata_->getLOOKUP("");
  
 
 
