9e750543a38f2a853a43fc9ce1456b2635fd3037
gaoxuan <253877113@qq.com>
2022-10-25 08:59:53 +0800

    test

--- src/fs/calvinfs_client_app.h
+++ src/fs/calvinfs_client_app.h
@@ -690,7 +690,7 @@ void LatencyExperimentAppend() {
     double start = GetTime();
     string from_path;
     string to_path;
-    for (int j = 0; j < 250; j++) {
+    for (int j = 0; j < 2; j++) {
       int a1 = rand() % 1000;
       int a2 = rand() % 1000;
       while (a2 == a1) {
@@ -724,7 +724,7 @@ void LatencyExperimentAppend() {
       BackgroundRenameFile(from_path,to_path) ;*/
 
       
-      from_path = "/a" + IntToString(machine()->machine_id()) + "/b" + IntToString(a1); 
+      from_path = "/a" + IntToString(machine()->machine_id()) + "/b" + IntToString(100); 
       to_path = "/a" + IntToString(rand() % machine()->config().size()) + "/d" + IntToString(machine()->GetGUID());
       LOG(ERROR)<<from_path <<"  renamed to   "<<to_path;
       BackgroundRenameFile(from_path,
