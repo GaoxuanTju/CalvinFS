14de6dc6fbfe928a08bcfbf893600f845f01bc6c
gaoxuan <253877113@qq.com>
2022-11-21 20:25:25 +0800

    LOG type

--- src/machine/connection/connection_zmq.cc
+++ src/machine/connection/connection_zmq.cc
@@ -140,8 +140,9 @@ void ConnectionZMQ::SendMessage(uint64 recipient, MessageBuffer* message) {
 
     Header *h = new Header();
     h->ParseFromArray(message->GetPart(0).buffer().data(), message->GetPart(0).buffer().size());
-    string s = h->rpc()+","+h->misc_string(0)+","+h->misc_string(1)+","+h->type();
-    LOG(ERROR)<<"content of msg is :"<<s;
+    string s = h->rpc()+","+h->misc_string(0)+","+h->misc_string(1);
+    LOG(ERROR)<<"content of msg is :"<<s<<"and"<<h->type();
+
   }
 
 
