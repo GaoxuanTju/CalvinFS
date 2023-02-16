e61ea8950f9145c9db1bfd191c7e489470d1537d
gaoxuan <253877113@qq.com>
2023-02-16 22:40:31 +0800

    debug

--- src/fs/calvinfs_client_app.h
+++ src/fs/calvinfs_client_app.h
@@ -165,9 +165,9 @@ class CalvinFSClientApp : public App {
     temp->set_app(name());
     temp->set_rpc("SUMMARY_RENAME");
  
-    temp->add_misc_string(header->misc_string(0));
+  /*  temp->add_misc_string(header->misc_string(0));
     temp->add_misc_string(header->misc_string(1));
-    /*temp->set_from_length(header->from_length());
+    temp->set_from_length(header->from_length());
     for(int i = 0; i < 8 ; i++)
     {
       temp->add_split_string_from(header->split_string_from(i));
