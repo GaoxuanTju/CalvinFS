76ee94c0d903501aaf9c877395d03b92d4cbebc9
gaoxuan <253877113@qq.com>
2023-02-20 13:52:45 +0800

    debug

--- src/fs/calvinfs_client_app.h
+++ src/fs/calvinfs_client_app.h
@@ -752,7 +752,7 @@ void LatencyExperimentAppend() {
 
     Spin(1);
     //gaoxuan --function Init() is used to initialize the metadata of dir and file which used for Rename 
-    dir_tree = new BTNode();
+    dir_tree = new BTNode;//老忘记，使用指针前，最少要指向一个地方
     metadata_->Init(dir_tree);//gaoxuan --Init() is in metadat_store.cc,参数用于存储目录树
     Spin(1);
     machine()->GlobalBarrier();
