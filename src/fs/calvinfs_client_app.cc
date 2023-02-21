124656fe782faea49b65652d7505ac92e26c5635
gaoxuan <253877113@qq.com>
2023-02-21 11:17:33 +0800

    debug

--- src/fs/calvinfs_client_app.cc
+++ src/fs/calvinfs_client_app.cc
@@ -486,7 +486,8 @@ void CalvinFSClientApp::rename_dir_tree(BTNode* &dir_tree, string from_path, str
   int pos = to_path.rfind('/');
   string parent_to_path = to_path.substr(0, pos);
   BTNode *to = find_path(dir_tree, parent_to_path, to_pre);
-
+  LOG(ERROR)<<from_path<<" "<<to_path<<" "<<from->path<<" "<<to->path;
+/*
   if (from != NULL && to != NULL)
   {
 
@@ -506,7 +507,7 @@ void CalvinFSClientApp::rename_dir_tree(BTNode* &dir_tree, string from_path, str
   {
     return;
   }
-  
+  */
 }
 void CalvinFSClientApp::copy_dir_tree(BTNode *dir_tree, string from_path, string to_path)
 {
