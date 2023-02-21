16b5414cfa9920ecfab16a19c83ea78654eb6435
gaoxuan <253877113@qq.com>
2023-02-21 10:38:18 +0800

    debug

--- src/fs/calvinfs_client_app.cc
+++ src/fs/calvinfs_client_app.cc
@@ -482,7 +482,7 @@ void CalvinFSClientApp::rename_dir_tree(BTNode* &dir_tree, string from_path, str
   BTNode *from_pre = NULL;
   BTNode *from = find_path(dir_tree, from_path, from_pre);
   LOG(ERROR)<<from_path<<"  "<<to_path;
-  LOG(ERROR)<<from->path;
+  //LOG(ERROR)<<from->path;
  // LOG(ERROR)<<from_pre->path;
   BTNode *to_pre = NULL;
   // 这块不对，不能直接把to_path放进去，因为可能rename到一个新位置，最次也得放ParentDir(to_path)
