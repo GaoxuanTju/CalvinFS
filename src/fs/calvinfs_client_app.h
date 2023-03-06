044edbfec992d8195b14103d3beb97b951da4dae
gaoxuan <253877113@qq.com>
2023-03-06 22:18:38 +0800

    debug

--- src/fs/calvinfs_client_app.h
+++ src/fs/calvinfs_client_app.h
@@ -972,7 +972,7 @@ public:
 
     double start = GetTime();
 
-    string from_path = "/a" + IntToString(machine()->machine_id()) + "/b" + IntToString(1);
+    string from_path = "/a" + IntToString(machine()->machine_id());
     BackgroundLS(from_path);
     LOG(ERROR) << "[" << machine()->machine_id() << "] "
                << "LS file " << from_path;
