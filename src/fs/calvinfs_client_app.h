c58e451dc14ddcce8d2121212e6cbb283081fd07
gaoxuan <253877113@qq.com>
2023-03-15 21:53:21 +0800

    test of tree_to_tree

--- src/fs/calvinfs_client_app.h
+++ src/fs/calvinfs_client_app.h
@@ -902,7 +902,7 @@ public:
       a5 = rand() % 2;
     }
     from_path = "/a_0" + IntToString(machine()->machine_id()) + "/a_1" + IntToString(a1) + "/a_2" + IntToString(a2) + "/a_3" + IntToString(a3) + "/a_4" + IntToString(a4);
-    to_path = "/a_0" + IntToString(rand() % machine()->config().size()) + "/a_1" + IntToString(a1) + "/a_2" + IntToString(a2) + "/a_3" + IntToString(a3) + "/A" + IntToString(machine()->GetGUID());
+    to_path = "/a_0" + IntToString(rand() % machine()->config().size()) + "/a_1" + IntToString(a1) + "/a_2" + IntToString(a2) + "/a_3" + IntToString(a5) + "/A" + IntToString(machine()->GetGUID());
     // 上面是到相同父目录下
     uint64 from_id = config_->LookupMetadataShard(config_->HashFileName(from_path), config_->LookupReplica(machine()->machine_id()));
     uint64 to_id = config_->LookupMetadataShard(config_->HashFileName(to_path), config_->LookupReplica(machine()->machine_id()));
