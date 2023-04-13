78c41535deb06d67f71ac14593063233104299e1
GaoxuanTju <253877113@qq.com>
2023-04-13 21:57:25 +0800

    6 7 -13

--- src/fs/calvinfs_client_app.h
+++ src/fs/calvinfs_client_app.h
@@ -1109,7 +1109,7 @@ public:
     //  BackgroundLS(path2);
       // BackgroundLS(path2);
       // BackgroundLS(path3);
-      BackgroundLS("/a2");
+      BackgroundLS("/a3");
       //sleep(1);
     }
 
