51af19d55bc4a7f9f1a779c6c5003634be0e2e86
gaoxuan <253877113@qq.com>
2023-04-26 19:01:23 +0800

    4,5 access 14

--- src/fs/calvinfs_client_app.h
+++ src/fs/calvinfs_client_app.h
@@ -1096,7 +1096,7 @@ public:
     machine()->GlobalBarrier();
     Spin(1);
     double start = GetTime();
-    string path1 = "/v0";
+    string path1 = "/a4";
     for (int j = 0; j < operation_num; j++)
     {
       BackgroundLS(path1);
